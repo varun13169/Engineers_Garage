6a98941efb8b5c5cbea95de204d506b5ab5877d9
varun13169 <varun13169@iiitd.ac.in>
2015-07-06 07:43:44 +0530

    small mistake

--- Controlling Mouse Pointer using Python Script and Accelerometer/python_serial.py
+++ Controlling Mouse Pointer using Python Script and Accelerometer/python_serial.py
@@ -1,18 +1,1 @@
-import serial # http://pyserial.sourceforge.net/ , Version: 2.7
-import autopy # http://autopy.org , Version: 0.51
-
-ser = serial.Serial('/dev/ttyUSB0',9600) # This is for Linux <ubuntu> for Windows select the respective COM port
-
-# My screen's resolution in pixels,
-# you might want to change it.
-# 1365x767
-
-
-while(1):
-	try:
-		accelerometerReading = map(int,ser.readline().split())
-		autopy.mouse.move(accelerometerReading[1],accelerometerReading[0])
-		print accelerometerReading
-	
-	except:
-		None
+a
